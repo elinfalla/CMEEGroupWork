038fcd2fdc3b18988e70c11dc8d6e1c457ea3ce5
Elin Falla <elinfalla@me.com>
2021-01-19 17:48:50 +0000

    Put all dirs and files into dir called Elin to prevent merge conflicts when merging

---
+++ Elin/Code/Vectorize2.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+""""Doctring here"""
+
+__appname__ = 
+#etc
+
+import numpy as np
+import sys
+import time
+
+# Runs the stochastic Ricker equation with gaussian fluctuations
+
+def stochrick(p0 = np.random.uniform(.5, 1.5, 1000), r = 1.2, K = 1, sigma = 0.2, numyears=100):
+  """""""
+  N = np.zeros((numyears, len(p0)))  #initialize empty matrix
+  
+  N[0] = p0
+  
+  for pop in range(0, len(p0)): # loop through the populations
+    
+    for yr in range(1, numyears): #for each pop, loop through the years
+      
+      N[yr, pop] = N[yr-1, pop] * np.exp(r * (1 - N[yr - 1, pop] / K) + np.random.normal(0, sigma, 1)) # add one fluctuation from normal distribution
+      
+  return N
+
+# Now write another function called stochrickvect that vectorizes the above 
+# to the extent possible, with improved performance: 
+
+def stochrickvect(p0 = np.random.uniform(.5, 1.5, 1000), r = 1.2, K = 1, sigma = 0.2,numyears=100):
+  """""""
+  N = np.zeros((numyears, len(p0))) # initialize empty matrix
+  
+  N[0] = p0
+  
+  for yr in range(1, numyears): #for each pop, loop through the years
+    
+    N[yr, ] =  N[yr-1, ] * np.exp(r * (1 - N[yr - 1, ] / K) + np.random.normal(0, sigma, 1)) # add one fluctuation from normal distribution
+      
+  return N
+
+def main(argv):
+  """""""
+  start = time.time()
+  T = stochrick()
+  end = time.time()
+  print("Non-vectorized Stochastic Ricker takes:")
+  print(end - start)
+
+  start = time.time()
+  T = stochrickvect()
+  end = time.time()
+  print("Vectorized Stochastic Ricker takes:")
+  print(end - start)
+
+  print("Script done!")
+
+  return 0
+
+if __name__ == "__main__":
+  status = main(sys.argv)
+  sys.exit(status)
