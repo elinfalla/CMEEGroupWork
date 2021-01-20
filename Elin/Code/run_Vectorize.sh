adc85d200b984694832f9b13976fa2fdc6aaf6f1
Elin Falla <elinfalla@me.com>
2021-01-20 10:42:33 +0000

    Modify run_Vectorise.sh

--- Elin/Code/run_Vectorize.sh
+++ Elin/Code/run_Vectorize.sh
@@ -10,20 +10,14 @@
 
 # Date: Oct 2020
 
-#chmod +x ./Vectorize1.R
-#chmod +x ./Vectorize2.R
 echo "------ VECTORIZE1.R OUTPUT ------"
 Rscript Vectorize1.R
-echo "------ END OF OUTPUT ------"
 
 echo "------ VECTORIZE2.R OUTPUT ------"
 Rscript ./Vectorize2.R
-echo "------ END OF OUTPUT ------"
 
 echo "------ VECTORIZE1.PY OUTPUT ------"
 python3 ./Vectorize1.py
-echo "------ END OF OUTPUT ------"
 
 echo "------ VECTORIZE2.PY OUTPUT ------"
 python3 ./Vectorize2.py
-echo "------ END OF OUTPUT ------"
